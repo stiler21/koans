6923bba7ace3e284c6a6d098835c40ece42770b3
stiler21 <jackgolubip@gmail.com>
2020-12-09 12:36:26 +0200

    Progress

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,20 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  def initialize()
+    @values = []
+  end
+  def roll(n)
+    @values = []
+    r = Random.new
+    n.times do
+      @values.push(r.rand(1..6))
+    end
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
